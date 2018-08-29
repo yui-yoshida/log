d50c4a735b8a7de3982b41553c15cb72963ee914
吉田唯 <yui.0618.kj8@gmail.com>
2018-08-29 06:11:17 +0000

    photo

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Log
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

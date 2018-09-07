220467254ca039cc322b92dec78023dc7bdda86d
github yui-yoshida <yui.0618.kj8@gmail.com>
2018-09-07 15:31:06 +0900

    ヘッダーにログインリンクを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
-      flash[:danger] = "ログインに失敗しました"
+      flash[:notice] = "ログインに失敗しました"
       render "new"
     end
   end

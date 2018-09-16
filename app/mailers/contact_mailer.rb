9e00cfdc6e35276b09be3f940cf09bf3fc15de1b
github yui-yoshida <yui.0618.kj8@gmail.com>
2018-09-16 12:17:27 +0900

    contact_mailer.rbをアソシエーションにより簡易に記述

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(blog)
-    @blog_user_email = blog.user.email
-    @blog_content = blog.content
-    mail to: @blog_user_email, subject: "Smile作成確認メール"
+    @blog = blog
+    mail to: @blog.user.email, subject: "Smile作成確認メール"
   end
 end
